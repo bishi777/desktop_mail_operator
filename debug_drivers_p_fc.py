b085a5ac309c76bd78d39b24deb96b87e7d63d4d
bishi777 <bishibashi414510@gmail.com>
2025-05-03 22:59:40 +0900

    auto commit

--- debug_drivers_p_fc.py
+++ debug_drivers_p_fc.py
@@ -9,7 +9,6 @@ import os
 import time
 import traceback
 
-
 user_data = func.get_user_data()
 wait_time = 1.5
 mailserver_address = user_data['user'][0]['gmail_account']
@@ -17,7 +16,6 @@ mailserver_password = user_data['user'][0]['gmail_account_password']
 receiving_address = user_data['user'][0]['user_email']
 pcmax_datas = user_data["pcmax"]
 # pcmax_datas = pcmax_datas[:9]
-# アカウントごとのデバッグポート＆プロファイルパス
 profiles = settings.chrome_user_profiles
 for p in profiles:
   print(f"🔁 操作中: {p['name']}")
@@ -29,37 +27,46 @@ for p in profiles:
   if driver.current_url != "https://pcmax.jp/pcm/member.php" and  driver.current_url != "https://pcmax.jp/pcm/index.php":
     continue
   name = ""
-  for index, i in enumerate(pcmax_datas):
-    if p['name'] == i['name']:
-      name = i["name"]
-      # if  "ゆっこ" != name:
-      #   print(name)
-      #   continue
-      login_id = i["login_id"]
-      login_pass = i["password"]
-      print(f"{login_id}   {login_pass}")
-      gmail_address = i["mail_address"]
-      gmail_password= i["gmail_password"]
-      fst_message = i["fst_mail"]
-      second_message = i["second_message"]
-      condition_message = i["condition_message"]
-  if name:
-    time.sleep(2)
-    send_cnt = 2
-    try:
-        print("新着メールチェック開始")     
-        pcmax_2.check_mail(name, driver, login_id, login_pass, gmail_address, gmail_password, fst_message, second_message, condition_message, mailserver_address, mailserver_password)
-        driver.get("https://pcmax.jp/pcm/member.php")
-    except Exception as e:
-      print(f"{name}❌ メールチェック  の操作でエラー: {e}")
-      traceback.print_exc() 
-    try:
-        print("fst_mail送信開始")
-        pcmax_2.set_fst_mail(name, driver, fst_message, send_cnt)
-        time.sleep(1.5)   
-    except Exception as e:
-      print(f"{name}❌ fst_mail  の操作でエラー: {e}")
-      traceback.print_exc()  
+
+  while True:
+    start_time = time.time() 
+    for index, i in enumerate(pcmax_datas):
+      if p['name'] == i['name']:
+        name = i["name"]
+        # if  "ゆっこ" != name:
+        #   print(name)
+        #   continue
+        login_id = i["login_id"]
+        login_pass = i["password"]
+        print(f"{login_id}   {login_pass}")
+        gmail_address = i["mail_address"]
+        gmail_password= i["gmail_password"]
+        fst_message = i["fst_mail"]
+        second_message = i["second_message"]
+        condition_message = i["condition_message"]
+    if name:
+      time.sleep(2)
+      send_cnt = 2
+      try:
+          print("新着メールチェック開始")     
+          pcmax_2.check_mail(name, driver, login_id, login_pass, gmail_address, gmail_password, fst_message, second_message, condition_message, mailserver_address, mailserver_password)
+          driver.get("https://pcmax.jp/pcm/member.php")
+      except Exception as e:
+        print(f"{name}❌ メールチェック  の操作でエラー: {e}")
+        traceback.print_exc() 
+      try:
+          print("fst_mail送信開始")
+          pcmax_2.set_fst_mail(name, driver, fst_message, send_cnt)
+          time.sleep(1.5)   
+      except Exception as e:
+        print(f"{name}❌ fst_mail  の操作でエラー: {e}")
+        traceback.print_exc()  
+
+    elapsed_time = time.time() - start_time  # 経過時間を計算する   
+    while elapsed_time < 600:
+      time.sleep(30)
+      elapsed_time = time.time() - start_time  # 経過時間を計算する
+      print(f"待機中~~ {elapsed_time} ")
 
   
 
