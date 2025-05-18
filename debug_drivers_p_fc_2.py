dfacaefad777d77969be1231de192a45f568d55b
bishi777 <bishibashi414510@gmail.com>
2025-05-18 19:43:04 +0900

    auto commit

--- debug_drivers_p_fc_2.py
+++ debug_drivers_p_fc_2.py
@@ -27,15 +27,14 @@ driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 10)
 handles = driver.window_handles
 
-# # DevTools Protocol で User-Agent を変更
+# DevTools Protocol で User-Agent を変更
 # driver.execute_cdp_cmd('Network.setUserAgentOverride', {
 #     "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1"
 # })
 # user_agent_type = "iPhone"
 # driver.refresh()
 # wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
-# time.sleep(0.5)
-user_agent_type = "PC"
+# time.sleep(1.5)
 
 print(f"タブ数: {len(handles)}")
 while True:
@@ -52,6 +51,7 @@ while True:
       "profile_list.php",
       "profile_detail.php",
       "profile_rest_reference.php",
+      "pcmax.jp/pcm/file.php"
     ]
     if any(part in driver.current_url for part in skip_urls):
       driver.get("https://pcmax.jp/pcm/index.php")
@@ -62,12 +62,10 @@ while True:
       continue
     try:
       login_flug = pcmax_2.catch_warning_pop("", driver)
-      print(login_flug)
-      if user_agent_type == "iPhone":
-        name_on_pcmax = driver.find_elements(By.CLASS_NAME, 'header-user-name')
-      elif user_agent_type == "PC":
-        name_on_pcmax = driver.find_elements(By.CLASS_NAME, 'mydata_name')
+      print(login_flug)   
+      name_on_pcmax = driver.find_elements(By.CLASS_NAME, 'mydata_name')
       while not len(name_on_pcmax):
+        # 再ログイン処理
         main_photo = driver.find_elements(By.CLASS_NAME, 'main_photo')
         if len(main_photo):
           print(8888888888888)
@@ -85,12 +83,8 @@ while True:
         login_button = driver.find_element(By.NAME, "login")
         login_button.click()
         wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
-        time.sleep(1.5)
-        pcmax_2.catch_warning_pop("", driver)
-        if user_agent_type == "iPhone":
-          name_on_pcmax = driver.find_elements(By.CLASS_NAME, 'header-user-name')
-        else:
-          name_on_pcmax = driver.find_elements(By.CLASS_NAME, 'mydata_name')
+        time.sleep(1.5)      
+        name_on_pcmax = driver.find_elements(By.CLASS_NAME, 'mydata_name')
         re_login_cnt = 0
         while not len(name_on_pcmax):
           time.sleep(5)
@@ -99,14 +93,12 @@ while True:
           wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
           time.sleep(1.5)
           pcmax_2.catch_warning_pop("", driver)
-          if user_agent_type == "iPhone":
-            name_on_pcmax = driver.find_elements(By.CLASS_NAME, 'header-user-name')
-          else:
-            name_on_pcmax = driver.find_elements(By.CLASS_NAME, 'mydata_name')
+          name_on_pcmax = driver.find_elements(By.CLASS_NAME, 'mydata_name')
           re_login_cnt += 1
           if re_login_cnt > 5:
             print("再ログイン失敗")
             break
+          
       name_on_pcmax = name_on_pcmax[0].text
       print(f"~~~~~~~~~~~~{name_on_pcmax}~~~~~~~~~~~~")
     except Exception as e:
@@ -120,7 +112,7 @@ while True:
       login_id = ""
       if name_on_pcmax == i['name']:
         name = i["name"]
-        # if  "りな" != name:
+        # if  "すい" != name:
         #   print(name)
         #   continue
         login_id = i["login_id"]
@@ -134,60 +126,8 @@ while True:
         send_cnt = 3
         try:
           print("新着メールチェック開始")   
-          if user_agent_type == "PC":
-            pcmax_2.check_mail(name, driver, login_id, login_pass, gmail_address, gmail_password, fst_message, second_message, condition_message, mailserver_address, mailserver_password, user_agent_type)
-            driver.get("https://pcmax.jp/pcm/index.php")
-          elif user_agent_type == "iPhone":
-            wait = WebDriverWait(driver, 10)
-            new_mail_lists = []
-            result = pcmax.check_new_mail(i, driver, wait)
-            if result is not None:
-                pcmax_new, return_foot_cnt = result
-            else:
-                pcmax_new, return_foot_cnt = 1, 0
-            if pcmax_new != 1:
-                new_mail_lists.append(pcmax_new)
-            # メール送信
-            smtpobj = None 
-            if len(new_mail_lists) == 0:
-                now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                print(f'{i["name"]}チェック完了  {now}')
-                pass
-            else:
-                # print("~~~~~~~~~~~~")
-                # print(f"{mailaddress} {gmail_password} {receiving_address}")
-                if mailserver_address and gmail_password and receiving_address:
-                    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    print(f'チェック完了 要確認メールあり  {now}')
-                    print(new_mail_lists) 
-                    text = ""
-                    subject = "新着メッセージ"
-                    for new_mail_list in new_mail_lists:
-                        # print('<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>')
-                        # print(new_mail_list)
-                        for new_mail in new_mail_list:
-                            text = text + new_mail + ",\n"
-                            if "警告" in text or "番号" in text:
-                                subject = "メッセージ"
-                try:
-                    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
-                    smtpobj.starttls()
-                    smtpobj.set_debuglevel(0)
-                    smtpobj.login(mailserver_address, gmail_password)
-                    msg = MIMEText(text)
-                    msg['Subject'] = subject
-                    msg['From'] = mailserver_address
-                    msg['To'] = receiving_address
-                    msg['Date'] = formatdate()
-                    smtpobj.send_message(msg)
-                except smtplib.SMTPDataError as e:
-                    print(f"SMTPDataError: {e}")
-                except Exception as e:
-                    print(f"An error occurred: {e}")
-                finally:
-                    if smtpobj: 
-                        smtpobj.close()   
-          
+          pcmax_2.check_mail(name, driver, login_id, login_pass, gmail_address, gmail_password, fst_message, second_message, condition_message, mailserver_address, mailserver_password, receiving_address)
+          driver.get("https://pcmax.jp/pcm/index.php")   
         except Exception as e:
           print(f"{name}❌ メールチェック  の操作でエラー: {e}")
           traceback.print_exc()  
@@ -195,7 +135,7 @@ while True:
           try:
             print("fst_mail送信開始")
             if send_cnt > 0:
-              pcmax_2.set_fst_mail(name, driver, fst_message, send_cnt, user_agent_type)
+              pcmax_2.set_fst_mail(name, driver, fst_message, send_cnt)
               time.sleep(1.5)   
           except Exception as e:
             print(f"{name}❌ fst_mail  の操作でエラー: {e}")
