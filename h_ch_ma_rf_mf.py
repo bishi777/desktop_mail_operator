414a8b96c7f848a30cc2e562f3624d7eafe53181
bishi777 <bishibashi414510@gmail.com>
2025-08-20 17:52:18 +0900

    auto commit

--- h_ch_ma_rf_mf.py
+++ h_ch_ma_rf_mf.py
@@ -124,6 +124,8 @@ try:
           except Exception as e:
             print(traceback.format_exc())
           # マッチング返し、
+          print(888)
+          print(returnfoot_flug)
           if report_dict[name] <= total_daily_limit and returnfoot_flug:
             try:
               return_foot_counted = happymail.return_footpoint(name, driver, wait, return_foot_message, matching_cnt, type_cnt, return_foot_cnt, return_foot_img, fst_message, matching_daily_limit, returnfoot_daily_limit, oneday_total_match, oneday_total_returnfoot)
