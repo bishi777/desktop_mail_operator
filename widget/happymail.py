0f16e1232bbc6fcf8e11c944d29d981a61ca1017
bishi777 <bishibashi414510@gmail.com>
2025-08-20 19:40:06 +0900

    auto commit

--- widget/happymail.py
+++ widget/happymail.py
@@ -308,7 +308,9 @@ def multidrivers_checkmail(name, driver, wait, login_id, password, return_foot_m
     driver.get("https://happymail.co.jp/sp/app/html/mbmenu.php")
     wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
     time.sleep(0.7)
-    catch_warning_screen(driver)
+    ban_flug = catch_warning_screen(driver)
+    if "利用できません" in ban_flug:
+      return "この登録は利用できません"
     new_message_flug = nav_item_click("メッセージ", driver, wait)
     # print(f"新着メールフラグ: {new_message_flug}")
     if new_message_flug == "新着メールなし":
