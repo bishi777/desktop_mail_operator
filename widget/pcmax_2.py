44ef464b44e6c808bd1f5b9e4ea5e39919d81455
bishi777 <bishibashi414510@gmail.com>
2025-09-29 17:00:33 +0900

    auto commit

--- widget/pcmax_2.py
+++ widget/pcmax_2.py
@@ -1020,8 +1020,8 @@ def return_footmessage(name, driver, return_foot_message, send_limit_cnt, mail_i
             time.sleep(1)
       elif len(driver.find_elements(By.CLASS_NAME, value='comp_title')):
         # print(77777777777)
-        print(driver.find_element(By.CLASS_NAME, value='comp_title').text)
-        if "送信完了" in driver.find_element(By.CLASS_NAME, value='comp_main_text').text:
+        # print(driver.find_element(By.CLASS_NAME, value='comp_title').text)
+        if "送信完了" in driver.find_element(By.CLASS_NAME, value='comp_title').text:
           rf_cnt += 1   
           print(f"{name} 足跡がえし マジ送信{maji_soushin} {user_n}  {rf_cnt}件送信  {now}")
           user_index += 1
