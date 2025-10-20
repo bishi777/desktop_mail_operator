1fec6c4d663edd5c24a7bf6cff33ad8e0ff83aa9
bishi777 <bishibashi414510@gmail.com>
2025-10-20 16:19:43 +0900

    auto commit

--- debug_drivers_p_ch_fm.py
+++ debug_drivers_p_ch_fm.py
@@ -216,10 +216,12 @@ while True:
     if wait_cnt % 6 == 0:
       print(f"待機中~~ {elapsed_time} ")
     wait_cnt += 1
-  print("🏁<<<<<<<<<<<<<ループ折り返し>>>>>>>>>>>>>>>>>>>>>🏁")
+  print("🎉🎉🎉<<<<<<<<<<<<<ループ折り返し>>>>>>>>>>>>>>>>>>>>>🎉🎉🎉")
   elapsed_time = time.time() - start_loop_time  # 経過時間を計算する   
   minutes, seconds = divmod(int(elapsed_time), 60)
-  print(f"タイム: {minutes}分{seconds}秒")  
+  print(f"タイム: {minutes}分{seconds}秒") 
+  print("🏁🏁🏁<<<<<<<<<<<<<🏃‍♀️>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>🏁🏁🏁")
+ 
   #カウント 
   roll_cnt += 1
   if roll_cnt % 6 == 0:
