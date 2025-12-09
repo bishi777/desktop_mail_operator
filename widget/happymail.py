17ffb6c47eb2a98011c1b811882d5175055717bc
bishi777 <bishibashi414510@gmail.com>
2025-12-09 16:43:21 +0900

    auto commit

--- widget/happymail.py
+++ widget/happymail.py
@@ -1419,10 +1419,10 @@ def return_type(name, wait, wait_time, driver, user_name_list, duplication_user,
     # 年齢チェック
     user_age = type_users[user_icon_type].find_element(By.CLASS_NAME, value="ds_like_list_age")
     # print(f"年齢チェック {user_age.text} {user_name}")
-    if not re.search(r"20代|18.?19", user_age.text):
-      # print("年齢が１０〜２０代ではないユーザー　スキップします")
-      user_icon_type += 1
-      continue
+    # if not re.search(r"20代|18.?19", user_age.text):
+    #   # print("年齢が１０〜２０代ではないユーザー　スキップします")
+    #   user_icon_type += 1
+    #   continue
 
     # タイプユーザーをクリック
     driver.execute_script("arguments[0].scrollIntoView({block: 'center', inline: 'center'});", type_users[user_icon_type])
