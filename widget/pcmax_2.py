f0422fcd76fda42d011ad93a9f838f50f3815c62
bishi777 <bishibashi414510@gmail.com>
2026-02-03 16:26:57 +0900

    auto commit

--- widget/pcmax_2.py
+++ widget/pcmax_2.py
@@ -219,6 +219,7 @@ def profile_search(driver, search_edit):
     "埼玉県": 24,
   }
   wait = WebDriverWait(driver, 10)
+  catch_warning_pop("", driver)
   if not "/mobile/profile_reference.php" in driver.current_url:
     if "/mobile/profile_rest_reference.php" in driver.current_url:
       print(f"❌ プロフ検索制限メニューのURLです") 
